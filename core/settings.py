077973bdb14434a6207d2454b701f1961fec77c3
Shikha409 <124184032+Shikha409@users.noreply.github.com>
2025-11-10 18:01:37 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -28,6 +28,12 @@ SECRET_KEY = 'django-insecure-x=qe5@^3%@t1fk)pk@uyv&r!z^#9==^*-&aiqfau3@9x@+j%nm
 DEBUG = True
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
+# CSRF & CORS Configuration for Railway Deployment
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-4e4f.up.railway.app',
+    'https://bot.nurostem.com',
+]
+
 
 # Application definition
 
